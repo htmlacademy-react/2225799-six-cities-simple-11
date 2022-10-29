bef84a21531de8c351a94921a38b4441ee2721f1
Tatiana Nurova <tv_nurova@betboom.org>
2022-10-29 18:15:32 +0300

    Маршрут по тайным тропам

---
+++ project/src/pages/404-page/404-page.tsx
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom';
+import Logo from '../../components/logo/logo';
+
+function Page404(): JSX.Element {
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Logo/>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--404">
+        <section className="page-404 container">
+          <h1>404. Page not found</h1>
+          <Link to="/">Вернуться на главную</Link>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Page404;
